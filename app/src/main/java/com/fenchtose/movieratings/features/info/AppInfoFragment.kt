28f720d81c6cabb367b52fa62e367b7d124e6bd2
Jay Rambhia <jayrambhia777@gmail.com>
2017-06-03 21:18:06 +0200

    share and rate events added

--- app/src/main/java/com/fenchtose/movieratings/features/info/AppInfoFragment.kt
+++ app/src/main/java/com/fenchtose/movieratings/features/info/AppInfoFragment.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.fenchtose.movieratings.BuildConfig
+import com.fenchtose.movieratings.MovieRatingsApplication
 import com.fenchtose.movieratings.R
+import com.fenchtose.movieratings.analytics.AnalyticsDispatcher
+import com.fenchtose.movieratings.analytics.events.Event
 import com.fenchtose.movieratings.base.BaseFragment
 import com.fenchtose.movieratings.base.RouterPath
 import com.fenchtose.movieratings.util.AccessibilityUtils
@@ -15,18 +18,25 @@ import com.fenchtose.movieratings.util.IntentUtils
 
 class AppInfoFragment: BaseFragment() {
 
+    private var analytics: AnalyticsDispatcher? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.info_page_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        analytics = MovieRatingsApplication.getAnalyticsDispatcher()
+
         view.findViewById(R.id.rate_view).setOnClickListener {
+            analytics?.sendEvent(Event("rate_app_clicked"))
             IntentUtils.openPlaystore(context)
         }
 
         view.findViewById(R.id.share_view).setOnClickListener {
-            IntentUtils.openShareIntent(context, "Have you met Flutter? It shows movie ratings on your screen when you're browsing Netflix. Get the app. " + IntentUtils.PLAYSTORE_URL)
+            analytics?.sendEvent(Event("share_app_clicked"))
+            IntentUtils.openShareIntent(context, "Have you met Flutter? It shows movie ratings on your screen when you're browsing Netflix. Get the app. https://goo.gl/y3HXVi")
         }
 
         view.findViewById(R.id.credit_view).setOnClickListener {
