d1699b432fc3b483ce7c5568b68182ca1362e2ef
Jay Rambhia <1256649+jayrambhia@users.noreply.github.com>
2018-08-31 23:51:50 +0200

    Fixed travis ci script

--- travis_ga_properties.sh
+++ travis_ga_properties.sh
@@ -1,8 +1,3 @@
-touch local.properties
-echo "omdb_api_key=\"\"" > local.properties
-echo "ratings_endpoint=\"\"" >> local.properties
-echo "ratings_api_key=\"\"" >> local.properties
-
 mkdir -p app/src/playstore/res/xml && touch app/src/playstore/res/xml/global_tracker.xml
 echo "<?xml version="1.0" encoding="utf-8"?>" > app/src/playstore/res/xml/global_tracker.xml
 echo "<resources>" >> app/src/playstore/res/xml/global_tracker.xml
