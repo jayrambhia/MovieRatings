4b8fe41f475000fe1cfad703c15fd8eb6b0e4e5d
Jay Rambhia <1256649+jayrambhia@users.noreply.github.com>
2018-12-30 13:11:25 +0100

    Fixed ReleaseKotlin error.
    DebugOptionsFragment did not have import statement for GaScreens.
    
    The package hierarchy was also incorrect so fixed that.

--- app/src/release/java/com/fenchtose/movieratings/features/debugging/DebugOptionsFragment.kt
+++ app/src/release/java/com/fenchtose/movieratings/features/debugging/DebugOptionsFragment.kt
@@ -2,6 +2,7 @@ package com.fenchtose.movieratings.features.debugging
 
 import com.fenchtose.movieratings.base.BaseFragment
 import com.fenchtose.movieratings.base.RouterPath
+import com.fenchtose.movieratings.analytics.ga.GaScreens
 
 class DebugOptionsFragment: BaseFragment() {
     override fun canGoBack() = true
