6e65ece6bc35ecf4f2f6cb7383ec7aadc6929e6e
Jay Rambhia <1256649+jayrambhia@users.noreply.github.com>
2018-12-30 23:54:41 +0100

    Settings Page - Individual item converted to RecyclerView.
    The page had a list of items in a LinearLayout. I converted the layout to a RecyclerView.
    
    This commit introduces SimpleAdapter. A quick-use RecyclerView apdater for the app.

--- app/src/main/java/com/fenchtose/movieratings/features/settings/SettingsFragment.kt
+++ app/src/main/java/com/fenchtose/movieratings/features/settings/SettingsFragment.kt
@@ -1,16 +1,20 @@
 package com.fenchtose.movieratings.features.settings
 
 import android.os.Bundle
-import android.support.annotation.IdRes
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.fenchtose.movieratings.R
 import com.fenchtose.movieratings.analytics.ga.GaCategory
 import com.fenchtose.movieratings.analytics.ga.GaScreens
 import com.fenchtose.movieratings.base.BaseFragment
 import com.fenchtose.movieratings.base.RouterPath
 import com.fenchtose.movieratings.features.settings.bubble.RatingBubbleSectionFragment
+import com.fenchtose.movieratings.widgets.SimpleAdapter
+import com.fenchtose.movieratings.widgets.SimpleAdapterViewBinder
 import com.fenchtose.movieratings.util.checkBatteryOptimized
 import com.fenchtose.movieratings.util.show
 
@@ -19,32 +23,92 @@ class SettingsFragment : BaseFragment() {
     override fun getScreenTitle() = R.string.settings_header
     override fun screenName() = GaScreens.SETTINGS
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.settings_page_redesign_layout, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        bindAction(view, R.id.settings_app_section, AppSectionFragment.SettingsAppSectionPath())
-        bindAction(view, R.id.settings_data_section, DataSectionFragment.DataSettingsPath())
-        bindAction(view, R.id.settings_tts_section, TTSSectionFragment.TTSSettingsPath())
-        bindAction(view, R.id.settings_misc_section, MiscSectionFragment.MiscSettingsPath())
-        bindAction(view, R.id.settings_rating_section, RatingBubbleSectionFragment.RatingSectionPath())
+    override fun onViewCreated(continar: View, savedInstanceState: Bundle?) {
+
+        val recyclerView: RecyclerView = continar.findViewById(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val adapter = SimpleAdapter(
+            listOf(
+                SimpleAdapterViewBinder(
+                    SettingsSection::class,
+                    R.layout.settings_page_section_item
+                ) { view, items, position ->
+                    bindItem(
+                        view,
+                        items[position] as SettingsSection
+                    )
+                }
+            )
+        )
+
+        recyclerView.adapter = adapter
+
+        val items = mutableListOf(
+            SettingsSection(
+                R.string.settings_app_section_title,
+                R.string.settings_app_section_subtitle,
+                AppSectionFragment.SettingsAppSectionPath()
+            ),
+            SettingsSection(
+                R.string.settings_data_and_privacy_section_title,
+                R.string.settings_data_and_privacy_section_subtitle,
+                DataSectionFragment.DataSettingsPath()
+            ),
+            SettingsSection(
+                R.string.settings_tts_section_title,
+                R.string.settings_tts_section_subtitle,
+                TTSSectionFragment.TTSSettingsPath()
+            ),
+            SettingsSection(
+                R.string.settings_misc_section_title,
+                R.string.settings_misc_section_subtitle,
+                MiscSectionFragment.MiscSettingsPath()
+            ),
+            SettingsSection(
+                R.string.settings_rating_section_title,
+                R.string.settings_rating_section_subtitle,
+                RatingBubbleSectionFragment.RatingSectionPath()
+            )
+        )
+
         if (checkBatteryOptimized(requireContext())) {
-            bindAction(view, R.id.section_battery_optimization, BatteryOptimizationPath())
+            items.add(
+                SettingsSection(
+                    R.string.settings_battery_optimization_section_title,
+                    R.string.settings_battery_optimization_section_subtitle,
+                    BatteryOptimizationPath()
+                )
+            )
+        }
+
+        adapter.setItems(items)
+    }
+
+    private fun bindItem(view: View, item: SettingsSection) {
+        view.findViewById<TextView>(R.id.settings_title).setText(item.title)
+        view.findViewById<TextView>(R.id.settings_subtitle).setText(item.subtitle)
+        view.setOnClickListener {
+            path?.getRouter()?.go(item.goTo)
         }
     }
 
-    private fun bindAction(root: View, @IdRes id: Int, goTo: RouterPath<out BaseFragment>) {
-        root.findViewById<View?>(id)?.apply {
-            show(true)
-            setOnClickListener {
-                path?.getRouter()?.go(goTo)
-            }
-        }
-    }
-
-    class SettingsPath: RouterPath<SettingsFragment>() {
+    class SettingsPath : RouterPath<SettingsFragment>() {
         override fun createFragmentInstance() = SettingsFragment()
         override fun category() = GaCategory.SETTINGS
     }
 }
+
+data class SettingsSection(
+    val title: Int,
+    val subtitle: Int,
+    val goTo: RouterPath<out BaseFragment>
+)
